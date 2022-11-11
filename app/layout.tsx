c3d3042063102d19aca6aebfa939a277455e2735
Suzan Rana <dev.suzanrana@gmail.com>
2022-11-11 20:10:10 +0545

    initial commit to suzanrana

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <head></head> 
+      <body>{children}
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Hello, this is my page made wiht NextJs.</div>
+  )
+}
+
+export default Home;
